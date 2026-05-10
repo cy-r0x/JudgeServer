e3275d8ad6c38e847b11e654df375bde48fe2c4f
Md Whahidul Islam Payel <whahidulislampayel@gmail.com>
2026-05-10 13:25:17 +0600

    Fix typo in route path for GetSubmission handler

--- handlers/submissions/routes.go
+++ handlers/submissions/routes.go
@@ -10,6 +10,6 @@ func (h *Handler) RegisterRoutes(mux *http.ServeMux, manager *middlewares.Manage
 	mux.Handle("GET /api/submissions", manager.With(h.ListUserSubmissions, middlewares.Authenticate))
 	mux.Handle("POST /api/submissions", manager.With(h.CreateSubmission, middlewares.Authenticate))
 	mux.Handle("PATCH /api/submissions", manager.With(h.UpdateSubmission, middlewares.AuthEngine))
-	mux.Handle("GET /api/submissions/{submissonId}", manager.With(h.GetSubmission, middlewares.Authenticate))
+	mux.Handle("GET /api/submissions/{submissionId}", manager.With(h.GetSubmission, middlewares.Authenticate))
 	mux.Handle("GET /api/submissions/all/{contestId}", manager.With(h.ListAllSubmissions, middlewares.Authenticate, middlewares.AuthenticateAdmin))
 }
